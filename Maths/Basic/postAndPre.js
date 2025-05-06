48726fad933fffa86acb1a096f4b7b9d43067925
Nikhil <aroranikhil8184@gmail.com>
2025-05-06 10:51:41 +0530

    post and pre operation

---
+++ Maths/Basic/postAndPre.js
@@ -0,0 +1,22 @@
+
+/**
+ * Post increment
+ * First we use the value and then second time we use the value is incrmented
+ * b=a++ we have use the value of a and assigned ot b then then a get incremeted
+ */
+let a=10
+
+let b=a++
+
+console.log({a,b})
+
+/**
+ * Pre increment
+ * First we incrmeent then use the value 
+ * b=++a we have incremented a first then uses the value of a and assigned to b
+ */
+let a2=10
+
+let b2=++a2
+
+console.log({a2,b2})
