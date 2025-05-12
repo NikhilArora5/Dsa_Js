d90acff53099611aac2e708167d6c80df1b6a9e0
Nikhil <aroranikhil8184@gmail.com>
2025-05-12 12:26:01 +0530

    factors of a number

---
+++ Maths/Basic/factorsOfNumbers.js
@@ -0,0 +1,16 @@
+/**
+ * Factors of a Number N 
+ * like how many numbers can divide N
+ * Two approaches 
+ * 1.Brute force loop till n and check for each (N%i==0)
+ * 2. Optimise loop till  n/2 only as the more than half of any number cannot divide that number so Math.floor(n/2) to handle odd cases of n
+ */
+
+
+for(let i=1;i<Math.floor(N/2);i++){
+
+}
+
+for(;;){
+    
+}
