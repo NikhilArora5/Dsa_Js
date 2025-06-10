c99fa268a140ca034ee44d06c22082e47740fc1e
Nikhil <aroranikhil8184@gmail.com>
2025-06-10 11:56:34 +0530

    Array two sum

--- Array/TwoSum.js
+++ Array/TwoSum.js
@@ -1,6 +1,7 @@
 const arr = [5, 1, 3, 7, 2];
 const sum = 10;
 
+//Solution where indices does not matter and need to check only whter possible
 const usingTwoPointer = (arr, sum) => {
   arr.sort((a, b) => a - b);
 
@@ -13,23 +14,17 @@ const usingTwoPointer = (arr, sum) => {
     if (leftPointer + rightPointer == sum) {
       return [rightPointer, leftPointer];
     } else if (rightPointer + leftPointer > sum) {
-        end = end - 1;
+      end = end - 1;
     } else {
-        start += 1;
+      start += 1;
     }
-
-
-
   }
 
-
-  return -1
+  return -1;
 };
 
-
-const result=usingTwoPointer(arr,sum)
-console.log("result",result)
-
+const result = usingTwoPointer(arr, sum);
+// console.log("result",result)
 
 /*
 For Multiple pairs
@@ -62,3 +57,21 @@ console.log(usingTwoPointer([1, 2, 3, 4, 5, 6], 7));
 
  */
 
+//Solution 2 where indices matter use hashmap
+const usingHashmap = (arr, k) => {
+  let hashmap = new Map();
+
+  for (let i = 0; i < arr.length; i++) {
+    let remElement = k - arr[i];
+
+    if (hashmap.has(remElement)) {
+      let index = hashmap.get(remElement);
+
+      return [index, i];
+    } else {
+      hashmap.set(arr[i], i);
+    }
+  }
+};
+
+console.log(usingHashmap([3,2,4],6))
