1367697c68c85a52495daa4310c8e796ea2fd7c8
NikhilArora5 <aroranikhil8184@gmail.com>
2026-01-31 16:00:05 +0530

    better code syntax and logic

--- Stack&Queus/NextGreaterElement/DailyTemperature.js
+++ Stack&Queus/NextGreaterElement/DailyTemperature.js
@@ -1,3 +1,31 @@
+
+//solution one better code and clean version
+var dailyTemperatures = function(arr) {
+    let stack = [];
+    let n = arr.length;
+    let ans = Array(n).fill(0);
+
+    stack.push(n-1);
+    for(let i = n-2; i >= 0; i--){
+        while(stack.length) {
+            let top = stack[stack.length-1];
+            if(arr[i] >= arr[top]) {
+                stack.pop();
+            } else {
+                ans[i] = top - i;
+                break;
+            }
+        }
+        stack.push(i);
+    }
+    return ans;
+};
+
+
+
+
+
+//solltuion2 : tried logic
 var dailyTemperatures = function (temperatures) {
     let result = []
     let stack = []
